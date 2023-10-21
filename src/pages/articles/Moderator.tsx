439e40be90af25e00ea84e1b0cca7036e9b17514
Sol-苏晨阳 <sol@fanruan.com>
2023-10-21 16:44:03 +0800

    dynamic

--- src/pages/articles/Moderator.tsx
+++ src/pages/articles/Moderator.tsx
@@ -1,84 +1,4 @@
-// import { GetStaticProps, NextPage } from "next";
-// import axios from "axios";
-// import { useState } from "react";
-// import ModeratorSortableTable from "../../components/table/ModeratorSortableTable";
-// import ColumnDropdown from "./ColumnDropdown";
-// import styles from "./ModeratorView.module.scss";
-
-// interface Article {
-//   id: string;
-//   title: string;
-//   authors: string;
-//   source: string;
-//   publication_year: string;
-//   doi: string;
-//   SE_practice: string;
-//   claim: string;
-//   evidence: string;
-//   approved: boolean;
-//   rejected: boolean;
-// }
-
-// interface ArticlesProps {
-//   articles: Article[];
-// }
-
-// const Articles: NextPage<ArticlesProps> = ({ articles }) => {
-//   const [activeTab, setActiveTab] = useState<'submitted' | 'approved' | 'rejected' | 'all'>('submitted');
-//   const [selectedColumns, setSelectedColumns] = useState<string[]>([
-//     "id", "title", "authors", "source", "publication_year",
-//     "doi", "SE_practice", "claim", "evidence", "approved", "rejected"
-//   ]);
-//   console.log("Hello");
-
-//   const headers = [
-//     { key: "title", label: "Title" },
-//     //... other headers
-//   ];
-
-//   const filterData = () => {
-//     switch (activeTab) {
-//       case 'submitted':
-//         return articles.filter(article => !article.approved && !article.rejected);
-//       case 'approved':
-//         return articles.filter(article => article.approved);
-//       case 'rejected':
-//         return articles.filter(article => article.rejected);
-//       default:
-//         return articles;
-//     }
-//   };
-
-//   return (
-//     <div className={styles.container}>
-//       {/* Rest of the JSX remains the same */}
-//       <ModeratorSortableTable
-//         headers={headers.filter((header) => selectedColumns.includes(header.key))}
-//         data={filterData()}
-//       />
-//     </div>
-//   );
-// };
-
-// export const getStaticProps: GetStaticProps<ArticlesProps> = async () => {
-//   try {
-//     console.log("okay 1");
-//           const response = await axios.get(
-//             "https://speed-1-pvgc460l8-notreallybenjamins-projects.vercel.app/api/articles"
-//           );
-//     const articles: Article[] = response.data;
-//     return { props: { articles } };
-//   } catch (error) {
-//     console.error("API data fetch error:", error);
-//     console.log("okay 2");
-//     return { props: { articles: [] } };
-//   }
-// };
-
-// export default Articles;
-// moderator.tsx
-
-import { GetStaticProps, NextPage } from "next";
+import { GetServerSideProps, GetStaticProps, NextPage } from "next";
 import data from "../../utils/dummydata.json";
 import ModeratorSortableTable from "../../components/table/ModeratorSortableTable";
 import { useState } from "react";
@@ -173,7 +93,7 @@ const Articles: NextPage<ArticlesProps> = ({ articles }) => {
   );
 };
 
-export const getStaticProps: GetStaticProps<ArticlesProps> = async (_) => {
+export const getServerSideProps: GetServerSideProps<ArticlesProps> = async (_) => {
 
   // Map the data to ensure all articles have consistent property names 
 
